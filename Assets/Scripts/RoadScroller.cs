a4660c8700cc42e754fb3fd635b971d72d7944d2
Antero Junior <anterojunior1530@gmail.com>
2025-11-27 19:27:21 -0300

    fix: scroll

--- Assets/Scripts/RoadScroller.cs
+++ Assets/Scripts/RoadScroller.cs
@@ -8,21 +8,16 @@ public class RoadScroller : MonoBehaviour
 
     private float loopPointY;
 
-    void Start()
-    {
-        loopPointY = -roadHeight;
-    }
-
     void Update()
     {
         foreach (Transform road in roadSections)
         {
             road.Translate(Vector3.down * scrollSpeed * Time.deltaTime);
 
-            if (road.position.y < -roadHeight)
+            if (road.position.y < -roadHeight) 
             {
-                float newYPosition = road.position.y + (roadHeight * 2f);
-                road.position = new Vector3(road.position.x, newYPosition, road.position.z);
+                float offset = roadHeight * 2f; 
+                road.position = new Vector3(road.position.x, road.position.y + offset, road.position.z);
             }
         }
     }
